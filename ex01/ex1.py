641f9fdcdedc84867ceec8c962c9808d63b85649
lusansan <loker.yu@gmail.com>
2019-09-29 20:19:51 +0800

    ex1

---
+++ ex01/ex1.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*- 
+# !/usr/bin/env python
+
+# @author: ‘loker‘
+# @software: PyCharm
+# @file: ex1.py
+# @time: 2019/9/29 20:16
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+
